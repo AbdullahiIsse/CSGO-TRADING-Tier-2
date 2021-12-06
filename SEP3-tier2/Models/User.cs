0444fb5a08f0fb5545759d3f7cf6633f9e019d95
lennartsargezian <Lennart_1997@hotmail.com>
2021-12-06 13:53:31 +0100

    securitylevel

--- SEP3-tier2/Models/User.cs
+++ SEP3-tier2/Models/User.cs
@@ -5,5 +5,7 @@ namespace SEP3_tier2.Models
         public long id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
+        
+        public long securitylevel { get; set; }
     }
 }
